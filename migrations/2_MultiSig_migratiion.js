6105ace3c6c8f865f4955c3912ff7d690b4e453d
nekantar-su <54645101+nekantar-su@users.noreply.github.com>
2022-05-04 12:06:59 -0400

    Fixing migration file for deployment

--- migrations/2_MultiSig_migratiion.js
+++ migrations/2_MultiSig_migratiion.js
@@ -1,5 +1,5 @@
 const multiSig = artifacts.require("MulitSig");
 
-module.exports = function (deployer,netowrk,accounts) {
+module.exports = function (deployer,network,accounts) {
   deployer.deploy(multiSig,1,[accounts[1],accounts[2]]);
 };
